a0eec8c3202cfa8b0b0b5402e9e31e09d6f6338f
h1063135843 <1063135843@qq.com>
2022-03-18 17:33:57 +0800

    update inference and visualize_confusion_matrix

--- tools/inference.py
+++ tools/inference.py
@@ -11,7 +11,9 @@ from mmdet3d.core.bbox.structures import points_cam2img
 from threading import Thread
 
 from mmseg.datasets import build_dataloader, build_dataset
+from mmseg.datasets.pipelines import ImageSegClassMapping, LoadAnnotations
 from mmseg.models import build_segmentor
+from mmseg.ops import resize
 
 
 def _load_points(pts_filename, with_color=False):
@@ -24,7 +26,18 @@ def _load_points(pts_filename, with_color=False):
     return points
 
 
-def get_feature_per_image(points, img_feature, depth2img):
+def load_annotation2d(ann_filename):
+    VALID_CLASS_IDS = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 24, 28,
+                       33, 34, 36, 39)
+    results = dict(ann_info=dict(seg_map=ann_filename), seg_fields=[])
+    load = LoadAnnotations(reduce_zero_label=False)
+    class_mapping = ImageSegClassMapping(VALID_CLASS_IDS, 40)
+    results = class_mapping(load(results))
+
+    return results['gt_semantic_seg']
+
+
+def get_feature_per_image(points, img_feature, depth, depth2img):
     h, w, c = img_feature.shape
     pts_2d = points_cam2img(points, depth2img, True)
 
@@ -33,8 +46,15 @@ def get_feature_per_image(points, img_feature, depth2img):
     yflag = torch.gt(y, 0) & torch.lt(y, h)
     zflag = torch.gt(z, 0)
     flag = xflag & yflag & zflag
+
+    z = (pts_2d[flag, 2] * 1000).long()
     pts_2d = pts_2d.long()
     x, y = pts_2d[flag, 0], pts_2d[flag, 1]
+    raw_z = depth[y, x]
+    depth_constrain = torch.gt(z, raw_z - 30) & torch.lt(z, raw_z + 30)
+    tmp=flag.clone()
+    flag[tmp] = depth_constrain
+    x, y = x[depth_constrain], y[depth_constrain]
 
     pts_img = torch.full([pts_2d.shape[0], c], 0).byte()
     pts_img[flag] = img_feature[y, x]
@@ -42,6 +62,7 @@ def get_feature_per_image(points, img_feature, depth2img):
 
 
 rootdir = r'G:\Datasets\scannet'
+label2d_dir = osp.join(rootdir, 'nyu_annotations', 'validation')
 
 
 def get_info(mode):
@@ -91,23 +112,40 @@ def get_points(scene, scan_list, info):
 mode = 'val'
 global_info = get_info(mode)
 global_list = get_scannet_list(mode)
-save_path = 'segformer_b0_points'
+save_path = 'ground'
+# save_path = '056800'
 mmcv.mkdir_or_exist(osp.join(rootdir, save_path))
 
 
 # arrays: h,w,c shape list of number_views length
-def save_logit_by_one_scene(scene, arrays, save=True):
+def save_logit_by_one_scene(scene,
+                            arrays,
+                            filelist,
+                            save=True,
+                            aggregation='max'):
     points = get_points(scene, global_list, global_info)
     depth2img = get_depth2img_by_scene_name(scene, global_list, global_info)
 
     pts_imgs = []
     for i, img in enumerate(arrays):
+        depth_path = osp.join(rootdir, 'images', 'depth',
+                              filelist[i].replace('jpg', 'png'))
+        depth = mmcv.imread(depth_path, 'unchanged')
+        depth = torch.from_numpy(depth.astype(
+            np.float)).unsqueeze(0).unsqueeze(0)
+        align_depth = resize(depth, img.shape[:2])
         pts_img = get_feature_per_image(
-            points, img, torch.tensor(depth2img[i], dtype=torch.float32))
+            points, img, align_depth[0][0],
+            torch.tensor(depth2img[i], dtype=torch.float32))
         pts_imgs.append(pts_img)
 
-    points_aug = torch.stack(pts_imgs, dim=0)
-    points_aug = points_aug.max(0)[0]
+    points_aug = torch.stack(pts_imgs, dim=0)  # n_view, n_points, c
+    if aggregation == 'max':
+        points_aug = points_aug.max(0)[0]
+    elif aggregation == 'avg':
+        points_sum = torch.sum(points_aug, 0, keepdim=False)
+        count = torch.count_nonzero(points_aug, 0)
+        points_aug = points_sum / count
 
     if save:
         points = np.concatenate([points, points_aug.numpy()], axis=1)
@@ -121,6 +159,8 @@ def parse_args():
     parser.add_argument('config', help='test config file path')
     parser.add_argument('checkpoint', help='checkpoint file')
     parser.add_argument('--mode', help='decided to infer which directory')
+    parser.add_argument(
+        '--ground', action='store_true', help='whether to use ground truth')
     args = parser.parse_args()
 
     return args
@@ -142,6 +182,7 @@ def main():
         global_list = get_scannet_list(mode)
     else:
         cur_scene = 'scene0011_00'
+        # cur_scene = 'scene0568_00'
 
     # build the dataloader
     dataset = build_dataset(cfg.data.test)
@@ -166,27 +207,49 @@ def main():
 
     seg_logits = []
     thread_list = []
+    ori_filelist = []
     for batch_indices, data in zip(loader_indices, data_loader):
-        scene = data['img_metas'][0].data[0][0]['ori_filename'][:12]
+        ori_file = data['img_metas'][0].data[0][0]['ori_filename']
+        scene = ori_file[:12]
         if cur_scene != scene:
             tmp = seg_logits.copy()
+            tmp2 = ori_filelist.copy()
             seg_logits.clear()
+            ori_filelist.clear()
 
-            t = Thread(target=save_logit_by_one_scene, args=(cur_scene, tmp))
+            t = Thread(
+                target=save_logit_by_one_scene, args=(cur_scene, tmp, tmp2))
             t.start()
             thread_list.append(t)
             cur_scene = scene
+        if args.ground:
+            label = load_annotation2d(
+                osp.join(label2d_dir, ori_file.replace('.jpg', '.png')))
 
-        with torch.no_grad():
-            result, seg_logit = model(return_loss=False, **data)
-        seg_logits.append(
-            (255 * seg_logit.squeeze(0).permute(1, 2, 0)).byte().cpu())
+            h, w = label.shape
+            cx, cy = torch.meshgrid(
+                torch.tensor(range(h)), torch.tensor(range(w)))
+            label = torch.tensor(label).reshape(-1)
+            c = label[label < 20].long()
+            cx = cx.reshape(-1)[label < 20].long()
+            cy = cy.reshape(-1)[label < 20].long()
 
-        batch_size = len(result)
+            seg_logit = torch.zeros((h, w, 20)).byte()
+            seg_logit = seg_logit.index_put_((cx, cy, c),
+                                             torch.ByteTensor([255]))
+            seg_logits.append(seg_logit)
+        else:
+            with torch.no_grad():
+                result, seg_logit = model(return_loss=False, **data)
+            seg_logits.append(
+                (255 * seg_logit.squeeze(0).permute(1, 2, 0)).byte().cpu())
+
+        ori_filelist.append(ori_file)
+        batch_size = len(result) if not args.ground else 1
         for _ in range(batch_size):
             prog_bar.update()
 
-    save_logit_by_one_scene(cur_scene, seg_logits)
+    save_logit_by_one_scene(cur_scene, seg_logits, ori_filelist)
 
     for t in thread_list:
         t.join()
--- tools/visualize_confusion_matrix.py
+++ tools/visualize_confusion_matrix.py
@@ -2,118 +2,134 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-# def show_confMat(confusion_mat, classes_name, set_name, out_dir,ax):
-#     """
-#     可视化混淆矩阵，保存png格式
-#     :param confusion_mat: nd-array
-#     :param classes_name: list,各类别名称
-#     :param set_name: str, eg: 'valid', 'train'
-#     :param out_dir: str, png输出的文件夹
-#     :return:
-#     """
 
-#     # 归一化
-#     confusion_mat_N = confusion_mat.copy()
-#     # confusion_mat_N = confusion_mat_N.astype('float') / confusion_mat_N.sum(axis=1)[:, np.newaxis]
-
-#     # 获取颜色
-#     cmap = plt.cm.get_cmap(
-#         'BuPu'
-#     )  # 更多颜色: http://matplotlib.org/examples/color/colormaps_reference.html
-#     im=ax.imshow(confusion_mat_N, cmap=cmap)
-
-
-#     # 设置文字
-#     xlocations = np.array(range(len(classes_name)))
-
-#     ax.xaxis.set_ticks_position('top') 
-#     plt.xticks(xlocations, classes_name, rotation=60,)
-#     plt.yticks(xlocations, classes_name)
-
-#     # 保存
-#     plt.savefig(os.path.join(out_dir, 'Confusion_Matrix_' + set_name + '.png'))
-    
 def norm(mat):
     confusion_mat_N = mat.copy()
-    confusion_mat_N = confusion_mat_N.astype('float') / confusion_mat_N.sum(axis=1)[:, np.newaxis]
+    confusion_mat_N = confusion_mat_N.astype('float') / confusion_mat_N.sum(
+        axis=1)[:, np.newaxis]
     return confusion_mat_N
 
 
+def show_conf_mat(data, classes_name, decimal=False):
+    num = len(data)
 
-data1=np.array([[21075636,   447976,   849189,   183406,    92624,    22837],
-        [  799558, 20719389,   371593,    38434,    15779,     4655],
-        [  649032,   203086, 14621448,  1887284,     4500,     5435],
-        [  149387,    45380,  1501524, 16811874,     1009,     2924],
-        [  106488,    16553,     5764,     1426,   664722,      480],
-        [  183227,   148438,     5197,      910,    19850,   334929]])
+    fig, ax = plt.subplots(1, num)
+    ax = ax.flatten() if num > 1 else [ax]
+    cmap = plt.cm.get_cmap('BuPu')
 
-data2=np.array([[21237527,   422226,   774217,   171876,    57830,     7992],
-        [  548330, 21131585,   212216,    44021,     9822,     3434],
-        [  669343,   190216, 14381328,  2127453,     2389,       56],
-        [  164992,    27690,  1271714, 17045566,      533,     1603],
-        [   91922,    16639,     3882,     1876,   680915,      199],
-        [  243447,   130210,     8483,     1004,     7799,   301608]])
+    xlocations = np.array(range(len(classes_name)))
+    for i in range(num):
+        majorFormatter = plt.FormatStrFormatter('%1.1f')
+        for j in range(data[i].shape[0]):
+            for k in range(data[i].shape[0]):
+                if decimal:
+                    txt = '%.3f' % data[i][k, j]
+                else:
+                    txt = format(data[i][k, j], ',').replace("-", u"\u2212")
+                ax[i].annotate(
+                    txt,
+                    xy=(j, k),
+                    horizontalalignment='center',
+                    verticalalignment='center',
+                    color='red')
+        im = ax[i].imshow(data[i], cmap=cmap)
+        ax[i].xaxis.set_ticks_position('top')
+        ax[i].set_xticks(xlocations)
+        ax[i].set_xticklabels(classes_name, rotation=60)
+        ax[i].set_yticks(xlocations)
+        ax[i].set_yticklabels(classes_name)
 
-data3=np.array([[21213732,   455588,   748926,   178289,    65023,    10110],
-        [  497497, 21232005,   164006,    43077,    11223,     1600],
-        [  730607,   227176, 14492509,  1912634,     3506,     4353],
-        [  172472,    37015,  1327723, 16971992,      861,     2035],
-        [   78748,    11804,     2126,     1464,   700958,      333],
-        [  200536,   187836,     3441,      592,    10572,   289574]])
-data=[data1,data2,data3]
-# # show_confMat(data1, ['imp surf', 'building', 'low_veg', 'tree', 'car', 'clutter'], "mit-b0", "./",1)
-# # show_confMat(data2, ['imp surf', 'building', 'low_veg', 'tree', 'car', 'clutter'], "mit-b4", "./",2)
+    fig.colorbar(im, ax=ax, fraction=0.045 / num, pad=0.05)
+    plt.show()
 
+
+def visualize_three_mats():
+    segformer = np.array([[21075636, 447976, 849189, 183406, 92624, 22837],
+                          [799558, 20719389, 371593, 38434, 15779, 4655],
+                          [649032, 203086, 14621448, 1887284, 4500, 5435],
+                          [149387, 45380, 1501524, 16811874, 1009, 2924],
+                          [106488, 16553, 5764, 1426, 664722, 480],
+                          [183227, 148438, 5197, 910, 19850, 334929]])
+
+    two_stream = np.array([[21237527, 422226, 774217, 171876, 57830, 7992],
+                           [548330, 21131585, 212216, 44021, 9822, 3434],
+                           [669343, 190216, 14381328, 2127453, 2389, 56],
+                           [164992, 27690, 1271714, 17045566, 533, 1603],
+                           [91922, 16639, 3882, 1876, 680915, 199],
+                           [243447, 130210, 8483, 1004, 7799, 301608]])
+
+    edft = np.array([[21213732, 455588, 748926, 178289, 65023, 10110],
+                     [497497, 21232005, 164006, 43077, 11223, 1600],
+                     [730607, 227176, 14492509, 1912634, 3506, 4353],
+                     [172472, 37015, 1327723, 16971992, 861, 2035],
+                     [78748, 11804, 2126, 1464, 700958, 333],
+                     [200536, 187836, 3441, 592, 10572, 289574]])
+    data = [segformer, two_stream, edft]
+    norm_data = [norm(x) for x in data]
+    dif_data = [data[1] - data[0], data[2] - data[0]]
+    classes_name = [
+        'imp surf', 'building', 'low_veg', 'tree', 'car', 'clutter'
+    ]
+
+    show_conf_mat(norm_data, classes_name, True)
+    show_conf_mat([data[2]], classes_name)
+    show_conf_mat(dif_data, classes_name)
+
+
+def visualize_scanet2d():
+    data = [[
+        111941090, 39305, 219221, 34627, 65072, 1441, 13197, 677, 11161,
+        304306, 1156923, 0, 228673, 0, 0, 1081468, 0, 31, 0, 1246174
+    ], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [
+                550071, 465, 34818, 1493281, 0, 271, 6995, 516, 145810, 0, 556,
+                0, 1699002, 0, 23012, 13599, 0, 2, 30253, 5266141
+            ],
+            [
+                670892, 6220, 875129, 7037864, 6124317, 266391, 92, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 92247, 4318939
+            ],
+            [
+                1766669, 22206, 0, 202, 326, 895717, 2097, 0, 0, 0, 269145, 0,
+                79452, 0, 1032, 3072, 4105, 2798, 2226, 5149185
+            ], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [
+                95452, 3577, 0, 0, 0, 0, 4927, 0, 609663, 0, 0, 0, 10450393,
+                9867, 299272, 0, 0, 0, 0, 901817
+            ],
+            [0, 0, 0, 0, 0, 0, 2, 0, 0, 157220, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [
+                409305, 1314663, 0, 0, 0, 116520, 8421975, 1748219, 84, 6353,
+                10916, 0, 484641, 0, 0, 637074, 0, 581, 662449, 10336002
+            ], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [
+                2703124, 11146, 0, 0, 0, 102125, 13883, 33666, 0, 748, 61296,
+                0, 565373, 0, 740199, 135, 0, 4, 1492, 5467908
+            ], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    data = np.array(data)
     
-# fig, ax =plt.subplots(2,2, frameon = False)
-# ax = ax.flatten() 
-# im=show_confMat(data2-data1, ['imp surf', 'building', 'low_veg', 'tree', 'car', 'clutter'], "mit-b4-fuse", "./",ax[0])
-# im=show_confMat(data3-data1, ['imp surf', 'building', 'low_veg', 'tree', 'car', 'clutter'], "mit-b4-fuse", "./",ax[1])
-# fig.colorbar(im, ax=[ax[0],ax[1]], fraction=0.03, pad=0.05)
-# plt.show()
+    norm_data=[norm(data)]
+    data=[data]
+    # print(data.shape)
 
-fig, ax = plt.subplots(1, 3)
-ax = ax.flatten()
- 
+    classes_name = [
+        'wall', 'floor', 'cabinet', 'bed', 'chair', 'sofa', 'table', 'door',
+        'window', 'bookshelf', 'picture', 'counter', 'desk', 'curtain',
+        'refrigerator', 'showercurtrain', 'toilet', 'sink', 'bathtub',
+        'otherfurniture'
+    ]
 
-cmap = plt.cm.get_cmap('BuPu') 
-classes_name=['imp surf', 'building', 'low_veg', 'tree', 'car', 'clutter']
-xlocations = np.array(range(len(classes_name)))
-
-
-# for i in range(2):
-#     # im = ax[i].imshow(norm(data[i]),cmap=cmap)
-#     im = ax[i].imshow(data[i+1]-data[0],cmap=cmap)
-#     ax[i].xaxis.set_ticks_position('top')
-#     ax[i].set_xticks(xlocations)
-#     ax[i].set_xticklabels(classes_name, rotation=60)
-#     ax[i].set_yticks(xlocations)
-#     ax[i].set_yticklabels(classes_name)
-
-for i in range(3):
-    # im = ax[i].imshow(norm(data[i]),cmap=cmap)
-    im = ax[i].imshow(norm(data[i]),cmap=cmap)
-    majorFormatter = plt.FormatStrFormatter('%1.1f') 
-    for j in range(data[i].shape[0]):
-        for k in range(data[i].shape[0]):
-            ax[i].annotate('%.3f'%norm(data[i])[k, j],xy=(j, k), horizontalalignment='center', verticalalignment='center')
-    ax[i].xaxis.set_ticks_position('top')
-    ax[i].set_xticks(xlocations)
-    ax[i].set_xticklabels(classes_name, rotation=60)
-    ax[i].set_yticks(xlocations)
-    ax[i].set_yticklabels(classes_name)
- 
-fig.colorbar(im, ax=[ax[0], ax[1],ax[2]], fraction=0.015, pad=0.05)
+    show_conf_mat(norm_data, classes_name, True)
+    # show_conf_mat(data, classes_name)
 
 
-
-
-# plt.imshow(data1, cmap=cmap)
-# ax=plt.gca()
-# ax.xaxis.set_ticks_position('top') 
-# plt.xticks(xlocations, classes_name, rotation=60,)
-# plt.yticks(xlocations, classes_name)
-# plt.colorbar()
-
-plt.savefig('tjn.png', bbox_inches='tight')
-plt.show()
+visualize_scanet2d()
+# visualize_three_mats()
